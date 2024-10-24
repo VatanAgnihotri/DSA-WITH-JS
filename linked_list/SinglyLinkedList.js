3d7795466fe66581f417cd68db9d11caeef28a37
Vatan Agnihotri <vatansharma07@gmail.com>
2024-10-24 23:44:39 +0530

    Singly Linked List Solution

--- linked_list/SinglyLinkedList.js
+++ linked_list/SinglyLinkedList.js
@@ -124,12 +124,77 @@ class SinglyLinkedList {
     }
     return this;
   }
+
+  // Question: Write a Method called findMiddle that returns the middle element of a singly linked list. If the list has an even number of elements, return the second middle element.
+  findMiddle() {
+    let slow = this.head;
+    let fast = slow;
+    let i = 0;
+    while (fast?.next) {
+      slow = slow.next;
+      fast = fast?.next?.next;
+    }
+    return slow;
+  }
+
+  //Question: Implement a Method called removeDuplicates to remove duplicate elements from a sorted linked list.
+  removeDuplicates() {
+    let temp = this.head;
+    while (temp?.next) {
+      if (temp.val === temp.next.val) {
+        temp.next = temp.next.next;
+        this.length--;
+      } else {
+        temp = temp.next;
+      }
+    }
+    return this.head;
+  }
+
+  //Question: Implement a Method called removeDuplicates to remove duplicate elements from an unsorted linked list.
+  removeDuplicatesUnsortedList() {
+    if (!this.head) return this;
+    const uniqueSet = new Set();
+    let temp = this.head;
+    let prv = null;
+    while (temp) {
+      if (!uniqueSet.has(temp.val)) {
+        uniqueSet.add(temp.val);
+        prv = temp;
+      } else {
+        prv.next = temp.next;
+        this.length--;
+      }
+      temp = temp.next;
+    }
+    this.tail = prv;
+    return this;
+  }
+
+  // Question: Write a Method called detectCycleStart that returns the node where a cycle begins in a linked list, if there is a cycle.
+  detectCycleStart() {
+    let slow = this.head;
+    let fast = this.head;
+    while (fast?.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (fast === slow) break;
+    }
+    if (!fast?.next) return null;
+    let entry = this.head;
+    while (entry !== slow) {
+      entry = entry.next;
+      slow = slow.next;
+    }
+    return entry;
+  }
 }
 
 const list = new SinglyLinkedList(10);
 list.push(20);
-list.push(22);
-console.log(`LIST :: ${JSON.stringify(list)}`);
+list.push(24);
+list.tail.next = list.head;
+console.log(`LIST :: ${list}`);
 
 // console.log(JSON.stringify(list.push(25)));
 // console.log(`POP:: ${JSON.stringify(list.pop())}`);
@@ -139,4 +204,8 @@ console.log(`LIST :: ${JSON.stringify(list)}`);
 // console.log(list.set(1, 12));
 // console.log(JSON.stringify(list.insert(3, 123)));
 // console.log(list.remove(1));
-console.log(`Reversed LIST :: ${JSON.stringify(list.reverse())}`);
+// console.log(`Reversed LIST :: ${JSON.stringify(list.reverse())}`);
+// console.log(list.findMiddle());
+// console.log(list.removeDuplicates());
+// console.log(list.removeDuplicatesUnsortedList());
+console.log(list.detectCycleStart());
