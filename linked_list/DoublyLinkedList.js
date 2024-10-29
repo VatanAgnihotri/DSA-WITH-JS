8236e635e74e977ef096a62f26493a16be1a6646
Vatan Agnihotri <vatansharma07@gmail.com>
2024-10-29 22:30:17 +0530

    Doubly Linked List

--- linked_list/DoublyLinkedList.js
+++ linked_list/DoublyLinkedList.js
@@ -72,12 +72,82 @@ class DoublyLinkedList {
     this.length--;
     return temp;
   }
+
+  get(index) {
+    if (!this.head || index >= this.length || index < 0) return undefined;
+    if (index === 0) return this.head;
+    let i = this.length - 1;
+    if (index === i) return this.tail;
+    const mid = i / 2;
+    let curr = this.tail;
+    if (index >= mid) {
+      while (i > index) {
+        curr = curr.prev;
+        i--;
+      }
+    } else {
+      curr = this.head;
+      let j = 0;
+      while (j < index) {
+        curr = curr.next;
+        j++;
+      }
+    }
+    return curr;
+  }
+
+  set(index, val) {
+    let curr = this.get(index);
+    if (!curr) return false;
+    curr.val = val;
+    return true;
+  }
+
+  insert(index, val) {
+    if (index === 0) {
+      this.unshift(val);
+      return true;
+    }
+    if (index === this.length - 1) {
+      this.push(val);
+      return true;
+    }
+    let next = this.get(index);
+    if (!next) return false;
+    const newNode = new Node(val);
+    const prev = next.prev;
+    newNode.next = next;
+    newNode.prev = prev;
+    prev.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let curr = this.get(index);
+    if (!curr) return false;
+    let next = curr.next;
+    let prev = curr.prev;
+    next.prev = prev;
+    prev.next = next;
+    curr.next = null;
+    curr.prev = null;
+    return curr;
+  }
 }
 
 const list = new DoublyLinkedList(10);
 list.push(20);
 list.push(30);
+list.push(40);
+list.push(50);
 // console.log("POP::", list.pop());
 // console.log(list.unshift(5));
-console.log("SHIFT ::", list.shift());
+// console.log("SHIFT ::", list.shift());
+// console.log("GET :: ", list.get(1));
+// console.log("SET :: ", list.set(0, 5));
+// console.log("INSERT :: ", list.insert(2, 5));
+console.log(list.remove(4));
 console.log("list::", list);
