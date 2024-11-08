c2f8d1412566e705734091a8de8b41ee519afbda
Vatan Agnihotri <vatansharma07@gmail.com>
2024-11-08 23:47:46 +0530

    Queue Constructor

---
+++ queue/Queue.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor(val) {
+    let newNode = new Node(val);
+    this.first = newNode;
+    this.last = newNode;
+    this.length = 1;
+  }
+}
