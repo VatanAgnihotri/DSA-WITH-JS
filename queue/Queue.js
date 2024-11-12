22fb7417633b5385d524b1232bf0cab91e5d2f42
Vatan Agnihotri <vatansharma07@gmail.com>
2024-11-13 01:36:23 +0530

    Queue Enqueue

--- queue/Queue.js
+++ queue/Queue.js
@@ -12,6 +12,23 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (this.length < 1) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = this.last.next;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let queue = new Queue(10);
+console.log("ENQUEUE :: ", queue.enqueue(20));
+queue.enqueue(30);
+console.log("DEQUE :: ", queue.deque());
+console.log("QUEUE :: ", queue);
