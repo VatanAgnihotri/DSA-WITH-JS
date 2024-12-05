3743a47c5b5ac76c9e0db0ffe14e4bbbae80e35e
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-05 23:46:42 +0530

    Hash Table Set Method

--- hash_table/HashTable.js
+++ hash_table/HashTable.js
@@ -10,8 +10,18 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    let index = this.hash(key);
+    if (!this.dataMap[index]) this.dataMap[index] = [];
+    this.dataMap[index].push([key, value]);
+    return this;
+  }
 }
 
 const myHash = new HashTable();
-
-console.log(myHash);
+myHash.set("NAME", "Zayn");
+myHash.set("Naam", "Zayn");
+myHash.set("Name", "Zayn")
+const set = myHash.set("Name", "Zayn");
+console.log(JSON.stringify(set));
