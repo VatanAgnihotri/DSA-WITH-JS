a14055b3b7c112a4cfca83be74181066aa68b47f
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-09 23:36:26 +0530

    Graph removeEdge method

--- graphs/Graph.js
+++ graphs/Graph.js
@@ -19,10 +19,27 @@ class Graph {
     }
     return false;
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (res) => res !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (res) => res !== vertex1
+      );
+      return true;
+    }
+    return false;
+  }
 }
 
 const graph = new Graph();
 graph.addVertex("A");
 graph.addVertex("B");
+graph.addVertex("C");
 graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+console.log(graph);
+graph.removeEdge("A", "B");
 console.log(graph);
