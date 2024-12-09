118b0bde0720cfe6b99e60268a8622a0e6ff8889
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-09 22:51:58 +0530

    Graph constructor and addVertex method

---
+++ graphs/Graph.js
@@ -0,0 +1,17 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      return true;
+    }
+    return false;
+  }
+}
+
+const graph = new Graph();
+graph.addVertex("A");
+console.log(graph);
