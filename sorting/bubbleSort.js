de2b9aceee5621e6d120019ac349649669fb6cc3
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-10 23:25:48 +0530

    Bubble Sort Solution 2

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,4 +1,5 @@
 const bubbleSort = (arr) => {
+//   Solution 1  
   let i = arr.length;
   let j = 0;
   while (j < i) {
@@ -14,6 +15,19 @@ const bubbleSort = (arr) => {
     }
   }
   return arr;
+
+//   Solution 2
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     for (let j = 0; j < i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+
+//   return arr;
 };
 
 console.log(bubbleSort([-2, 45, 0, 11, -9]));
