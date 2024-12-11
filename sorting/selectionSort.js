421ef70d7d3336a5e3c469bc66f01a66126212f0
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-12 00:53:22 +0530

    Selection Sort Solution

---
+++ sorting/selectionSort.js
@@ -0,0 +1,16 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    if (i !== min) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+};
+
+console.log(selectionSort([3, 1, 0, 4, 2]));
