ef82b9bdaa88edd50f2ad2981560e2292cf6c836
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-19 00:59:44 +0530

    validAnagram Solution 2

--- arrays and hashing/validAnagram.js
+++ arrays and hashing/validAnagram.js
@@ -12,22 +12,36 @@
 // Output: false
 
 const validAnagram = (s, t) => {
-  const hashMap1 = new Map();
-  const hashMap2 = new Map();
+  // const hashMap1 = new Map();
+  // const hashMap2 = new Map();
+  // if (s.length !== t.length) return false;
+  // for (let i = 0; i < s.length; i++) {
+  //   hashMap1.has(s[i])
+  //     ? hashMap1.set(s[i], hashMap1.get(s[i]) + 1)
+  //     : hashMap1.set(s[i], 1);
+  //   hashMap2.has(t[i])
+  //     ? hashMap2.set(t[i], hashMap2.get(t[i]) + 1)
+  //     : hashMap2.set(t[i], 1);
+  // }
+
+  // for (let [key, value] of hashMap1.entries()) {
+  //   if (!hashMap2.has(key) || hashMap2.get(key) !== value) return false;
+  // }
+  // return true;
+
+  //Solution 2
   if (s.length !== t.length) return false;
+  const hashMap = {};
   for (let i = 0; i < s.length; i++) {
-    hashMap1.has(s[i])
-      ? hashMap1.set(s[i], hashMap1.get(s[i]) + 1)
-      : hashMap1.set(s[i], 1);
-    hashMap2.has(t[i])
-      ? hashMap2.set(t[i], hashMap2.get(t[i]) + 1)
-      : hashMap2.set(t[i], 1);
+    hashMap[s[i]] = (hashMap[s[i]] || 0) + 1;
+    hashMap[t[i]] = (hashMap[t[i]] || 0) - 1;
   }
 
-  for (let [key, value] of hashMap1.entries()) {
-    if (!hashMap2.has(key) || hashMap2.get(key) !== value) return false;
+  for (let count in hashMap) {
+    if (hashMap[count] !== 0) return false;
   }
+
   return true;
 };
 
-// console.log(validAnagram("anagram", "nagaram"));
+console.log(validAnagram("anagram", "nagaram"));
