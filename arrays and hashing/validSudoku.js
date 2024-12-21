2f2e993df5b06c613330efe3ee6db3bc8f921f59
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-21 13:19:35 +0530

    Valid Sudoku better Solution

--- arrays and hashing/validSudoku.js
+++ arrays and hashing/validSudoku.js
@@ -38,36 +38,59 @@ Input: board = [
 // Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 
 const isValidSudoku = (board) => {
-  const rowsMap = new Map();
-  const colsMap = new Map();
-  const squareMap = new Map();
+  //Solution 1
+  // const rowsMap = new Map();
+  // const colsMap = new Map();
+  // const squareMap = new Map();
 
-  for (let i = 0; i < board.length; i++) {
+  // for (let i = 0; i < board.length; i++) {
+  //   for (let j = 0; j < 9; j++) {
+  //     if (board[i][j] === ".") continue;
+  //     if (
+  //       (rowsMap.has(i) && rowsMap.get(i).has(board[i][j])) ||
+  //       (colsMap.has(j) && colsMap.get(j).has(board[i][j])) ||
+  //       (squareMap.has(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`) &&
+  //         squareMap
+  //           .get(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`)
+  //           .has(board[i][j]))
+  //     )
+  //       return false;
+  //     rowsMap.has(i)
+  //       ? rowsMap.get(i).add(board[i][j])
+  //       : rowsMap.set(i, new Set([board[i][j]]));
+  //     colsMap.has(j)
+  //       ? colsMap.get(j).add(board[i][j])
+  //       : colsMap.set(j, new Set([board[i][j]]));
+  //     squareMap.has(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`)
+  //       ? squareMap
+  //           .get(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`)
+  //           .add(board[i][j])
+  //       : squareMap.set(
+  //           `(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`,
+  //           new Set([board[i][j]])
+  //         );
+  //   }
+  // }
+  // return true;
+
+  //Solution 2
+  let hashSet = new Set();
+  for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      if (board[i][j] === ".") continue;
-      if (
-        (rowsMap.has(i) && rowsMap.get(i).has(board[i][j])) ||
-        (colsMap.has(j) && colsMap.get(j).has(board[i][j])) ||
-        (squareMap.has(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`) &&
-          squareMap
-            .get(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`)
-            .has(board[i][j]))
-      )
-        return false;
-      rowsMap.has(i)
-        ? rowsMap.get(i).add(board[i][j])
-        : rowsMap.set(i, new Set([board[i][j]]));
-      colsMap.has(j)
-        ? colsMap.get(j).add(board[i][j])
-        : colsMap.set(j, new Set([board[i][j]]));
-      squareMap.has(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`)
-        ? squareMap
-            .get(`(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`)
-            .add(board[i][j])
-        : squareMap.set(
-            `(${Math.floor(i / 3)}, ${Math.floor(j / 3)})`,
-            new Set([board[i][j]])
-          );
+      const cellVal = board[i][j];
+      if (cellVal !== ".") {
+        if (
+          hashSet.has(`Row,${i},${cellVal}`) ||
+          hashSet.has(`Col,${j},${cellVal}`) ||
+          hashSet.has(
+            `Box,${Math.floor(i / 3)},${Math.floor(j / 3)},${cellVal}`
+          )
+        )
+          return false;
+        hashSet.add(`Row,${i},${cellVal}`);
+        hashSet.add(`Col,${j},${cellVal}`);
+        hashSet.add(`Box,${Math.floor(i / 3)},${Math.floor(j / 3)},${cellVal}`);
+      }
     }
   }
   return true;
