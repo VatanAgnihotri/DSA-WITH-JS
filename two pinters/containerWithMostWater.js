3f9529dcb82560a75371d693c471f49e2379ecdd
Vatan Agnihotri <vatansharma07@gmail.com>
2024-12-25 23:00:05 +0530

    container With most water bettwe solution

--- two pinters/containerWithMostWater.js
+++ two pinters/containerWithMostWater.js
@@ -25,10 +25,10 @@
 var maxArea = function (height) {
   let l = 0;
   let r = height.length - 1;
-  let water = 0;
-  while (l < r) {
-    water = Math.max(water, Math.min(height[l], height[r]) * (r - l));
-    height[l] === Math.min(height[l], height[r]) ? l++ : r--;
+  let maxArea = 0;
+  while (l <= r) {
+    maxArea = Math.max(maxArea, (r - l) * Math.min(height[l], height[r]));
+    height[l] < height[r] ? l++ : r--;
   }
-  return water;
+  return maxArea;
 };
